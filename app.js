dd72479560b78c6cd54ff0bcead912e4b56718c0
FlorianSdorra <fsd114@posteo.de>
2019-10-31 17:08:12 +0100

    added recordsRouter use

--- app.js
+++ app.js
@@ -40,6 +40,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 /** ROUTES */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/records', recordsRouter);
 
 /** EXPORT PATH */
 module.exports = app;
